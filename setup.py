70d42814fc1804720933f576b990d6cb7d0c3bb8
David Brodsky <davidpbrodsky@gmail.com>
2016-03-08 10:45:19 -0800

    update setup.py for 0.1 pypi release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 
 try:
     from setuptools import setup
@@ -14,22 +14,18 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
-requirements = [
-    'boto3',
-]
-
-test_requirements = [
-    'boto3',
-]
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='storage_provisioner',
     version='0.1.0',
     description="Provisions storage on pluggable backends, such as AWS S3.",
     long_description=readme + '\n\n' + history,
-    author="Chris Ballinger",
-    author_email='chrisballinger@gmail.com',
+    author="Chris Ballinger, David Brodsky",
+    author_email='chrisballinger@gmail.com, dbro@dbro.pro',
     url='https://github.com/PerchLive/storage_provisioner',
+    download_url='https://github.com/PerchLive/storage_provisioner/tarball/0.1',
     packages=[
         'storage_provisioner',
     ],
@@ -39,7 +35,7 @@ setup(
     install_requires=requirements,
     license="Apache License 2.0",
     zip_safe=False,
-    keywords='storage_provisioner',
+    keywords='storage_provisioner, aws, boto',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -49,5 +45,5 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=requirements
 )
